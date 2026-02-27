693c1f95177f12d02f006c91dc955447efa379ff
pavanhamsa <pavanhamsa2023@gmail.com>
2026-02-27 20:29:38 +0530

    ansible

--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -1,9 +1,6 @@
-terraform {
-  backend "s3" {
+
 bucket         = "remote-state-hamsa-dev" # Replace with your bucket name
 key            = "remote-state.tfstate"      # Desired state file name/path
 region         = "us-east-1"                     # Replace with your AWS region
 encrypt        = true                            # Enable encryption
 use_lockfile = true
-  }
-}
